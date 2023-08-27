d0d925e2a947dbbf12c88bd22b97a3fac4674403
Mayank <76843683+MATRIX-02@users.noreply.github.com>
2023-08-27 19:01:30 +0530

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,22 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
-const Nav = ({setLibraryStatus, libraryStatus})=>{
-    const openLibraryHandler = () => {
-        setLibraryStatus(!libraryStatus);
-      };
-    return(
-        <nav>
-            <h1>JustChill</h1>
-            <button
+const Nav = ({ setLibraryStatus, libraryStatus }) => {
+  const openLibraryHandler = () => {
+    setLibraryStatus(!libraryStatus);
+  };
+
+  return (
+    <nav>
+      <h1>JustChill</h1>
+      <button
         className={libraryStatus ? "library-active" : ""}
         onClick={openLibraryHandler}
       >
-                Library
-                <FontAwesomeIcon icon={faMusic}/>
-            </button>
-        </nav>
-    );
+        Library
+        <FontAwesomeIcon icon={faMusic}></FontAwesomeIcon>
+      </button>
+    </nav>
+  );
 };
 
 export default Nav;
