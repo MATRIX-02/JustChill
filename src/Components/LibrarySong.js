0a60062fa5e70125be0b049ed666f9fb90670499
Mayank <76843683+MATRIX-02@users.noreply.github.com>
2023-08-27 19:01:44 +0530

    Update LibrarySong.js

--- src/Components/LibrarySong.js
+++ src/Components/LibrarySong.js
@@ -1,46 +1,52 @@
 import React from "react";
-import {playAudio} from "../util";
+import { playAudio } from "../util";
 
-const LibrarySong = ({name, artist,setSongs,  cover, song, songs, setCurrentSong, id, audioRef, isPlaying}) =>{
+const LibrarySong = ({
+  name,
+  artist,
+  cover,
+  id,
+  setCurrentSong,
+  songs,
+  audioRef,
+  isPlaying,
+  setSongs,
+  active,
+}) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === id);
+    setCurrentSong({ ...selectedSong[0] });
+    //Set Active in library
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
 
-    const songSelectHandler = () =>{
-        const selectedSong = songs.filter((state) => state.id === id);
-        // const selectedSong = song;
-        setCurrentSong({...selectedSong[0]});
-        // setCurrentSong(song);
-
-        // add active state
-        const newSongs = songs.map((song) =>{
-            if(song.id === id){
-                return{
-                    ...song,
-                    active: true,
-                };
-            }
-            else{
-                return{
-                    ...song,
-                    active: false,
-                };
-            }
-        });
-
-        setSongs(newSongs);
-
-        // Check if the song is playing
-        playAudio(isPlaying, audioRef);
-        
-    }
-
-    return(
-        <div onClick={songSelectHandler} className={`library-song ${song.active ? "selected": ""}`}>
-            <img alt={name} src={cover}></img>
-            <div className="song-description">
-            <h3>{name}</h3>
-            <h4>{artist}</h4>
-            </div>  
-        </div> 
-    )
-}
+    //Play audio
+    playAudio(isPlaying, audioRef);
+  };
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${active ? "selected" : ""}`}
+    >
+      <img src={cover} alt="" />
+      <div className="song-description">
+        <h3>{name}</h3>
+        <h4>{artist}</h4>
+      </div>
+    </div>
+  );
+};
 
 export default LibrarySong;
