21fee43207ecf65612f4c52cd9560b89676dfca2
Mayank <76843683+MATRIX-02@users.noreply.github.com>
2023-08-27 19:00:17 +0530

    Update song.js

--- src/Components/song.js
+++ src/Components/song.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-
-const Song = ({currentSong, isPlaying}) =>{
-
-
-
-    return(
-        <div className="song-container">
-            <img alt={currentSong.name} src={currentSong.cover} className={isPlaying ? "rotateSong": ""}></img>
-            <h2>{currentSong.name}</h2>
-            <h3>{currentSong.artist}</h3>
-        </div>
-    );
+const Song = ({ currentSong, isPlaying }) => {
+  return (
+    <div className="song-container">
+      <img
+        className={isPlaying ? "rotateSong" : ""}
+        src={currentSong.cover}
+        alt=""
+        draggable="false"
+      />
+      <h2>{currentSong.name}</h2>
+      <h3>{currentSong.artist}</h3>
+    </div>
+  );
 };
 
 export default Song;
