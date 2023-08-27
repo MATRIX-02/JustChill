fe0f969fe4a1e5c2e13bea4e95665900ba1a75af
Mayank <76843683+MATRIX-02@users.noreply.github.com>
2023-08-27 19:01:02 +0530

    Update library.js

--- src/Components/library.js
+++ src/Components/library.js
@@ -1,24 +1,31 @@
 import React from "react";
+
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ libraryStatus, songs, setCurrentSong, audioRef, isPlaying, setSongs }) => {
+const Library = ({
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
   return (
-    <div className={`library ${libraryStatus ? "active-library" : ""}`}>
+    <div className={`library ${libraryStatus ? "active-library" : " "}`}>
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
           <LibrarySong
-            isPlaying={isPlaying}
             songs={songs}
-            audioRef={audioRef}
-            setCurrentSong={setCurrentSong}
-            song={song}
-            id={song.id}
-            key={song.id}
-            cover= {song.cover}
-            active = {song.active}
+            cover={song.cover}
             name={song.name}
-            artist = {song.artist}
+            artist={song.artist}
+            active={song.active}
+            key={song.id}
+            id={song.id}
+            setCurrentSong={setCurrentSong}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
             setSongs={setSongs}
           />
         ))}
