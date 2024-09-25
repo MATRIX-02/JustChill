b8f97b91e23f48a2f7a0492cf593f07736ff22e8
Mayank <mayanksingh122001@gmail.com>
2024-09-26 02:24:40 +0530

    fix: Music cover image updated

--- src/data.jsx
+++ src/data.jsx
@@ -1,129 +1,128 @@
 import { v4 as uuidv4 } from "uuid";
 
 function chillHop() {
-
-  return [
-    {
-      name: "Beautiful",
-      artist: "C Y G N ",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/06/9180a7fabdc07fe2e221bb26bdd477cb66f6b69e-1024x1024.jpg",
-      id: uuidv4(),
-      active: true,
-      color: ["#CF291B", "#BA8B39"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=60571",
-    },
-    {
-      name: "Take it Eazy",
-      artist: "C Y G N ",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/06/077e7c815c60119b2d48586aef12c406f4f525a1-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#291715", "#794631"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=60617",
-    },
-    {
-      name: "South of the Circle – OST ",
-      artist: "ChillHop",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/08/3f5eb74c8d8e36a38baa2f71be53ea5761ed3c6d-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FCC990", "#FE5052"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=62370",
-    },
-    {
-      name: "It’s Okay",
-      artist: "Yasper",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/05/5683fa8ae05221c0d06eeedd90fe00e653d27c48-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#8F6650", "#FCEFA0"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=55163",
-    },
-    {
-      name: "Lost Woodse",
-      artist: "cocabona, Emperose",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/05/1bcec532a76700819b9ca2853c175cdc54bccb02-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#513919", "#B48161"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=55130",
-    },
-    {
-      name: "Lookaway",
-      artist: "ØDYSSEE, Blumen",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/09/370fcf77d4ba59808c1109403c537e0fcc0b4edd-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#C6BAA4", "#3A3861"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=44900",
-    },
-    {
-      name: "Beaver Creek",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Daylight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Keep Going",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Nightfall",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: false,
-    },
-  ];
+	return [
+		{
+			name: "Beautiful",
+			artist: "C Y G N ",
+			cover:
+				"https://cms.chillhop.com/media/73119/squarefaa3720e3ef276d744bf371d8a0d66cb24746862.jpg",
+			id: uuidv4(),
+			active: true,
+			color: ["#CF291B", "#BA8B39"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=60571",
+		},
+		{
+			name: "Take it Eazy",
+			artist: "C Y G N ",
+			cover:
+				"https://cms.chillhop.com/media/60878/square9180a7fabdc07fe2e221bb26bdd477cb66f6b69e.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#291715", "#794631"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=60617",
+		},
+		{
+			name: "South of the Circle – OST ",
+			artist: "ChillHop",
+			cover:
+				"https://cms.chillhop.com/media/43688/squarel3f5eb74c8d8e36a38baa2f71be53ea5761ed3c6d.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#FCC990", "#FE5052"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=62370",
+		},
+		{
+			name: "It’s Okay",
+			artist: "Yasper",
+			cover:
+				"https://cms.chillhop.com/media/58882/squareb839fe4e2f03050732ec0aa305a10554420fb7de.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#8F6650", "#FCEFA0"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=55163",
+		},
+		{
+			name: "Lost Woodse",
+			artist: "cocabona, Emperose",
+			cover:
+				"https://cms.chillhop.com/media/58871/squarel1bcec532a76700819b9ca2853c175cdc54bccb02.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#513919", "#B48161"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=55130",
+		},
+		{
+			name: "Lookaway",
+			artist: "ØDYSSEE, Blumen",
+			cover:
+				"https://cms.chillhop.com/media/43827/squarel370fcf77d4ba59808c1109403c537e0fcc0b4edd.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#C6BAA4", "#3A3861"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=44900",
+		},
+		{
+			name: "Beaver Creek",
+			cover:
+				"https://cms.chillhop.com/media/10010/square0255e8b8c74c90d4a27c594b3452b2daafae608d.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Daylight",
+			cover:
+				"https://cms.chillhop.com/media/6799/squareef95e219a44869318b7806e9f0f794a1f9c451e4.jpg",
+			artist: "Aiguille",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Keep Going",
+			cover:
+				"https://cms.chillhop.com/media/12212/square7e98d3028a22ee7f16f6a9bfcdc2089f089777a5.jpg",
+			artist: "Swørn",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Nightfall",
+			cover:
+				"https://cms.chillhop.com/media/6799/squareef95e219a44869318b7806e9f0f794a1f9c451e4.jpg",
+			artist: "Aiguille",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Reflection",
+			cover:
+				"https://cms.chillhop.com/media/6816/squareff35dede32321a8aa0953809812941bcf8a6bd35.jpg",
+			artist: "Swørn",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Under the City Stars",
+			cover:
+				"https://cms.chillhop.com/media/10010/square0255e8b8c74c90d4a27c594b3452b2daafae608d.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+	];
 }
 
 export default chillHop;
